7fa9e862a95be908c5d68f1824b31ae866dbd786
Al1moff018 <alimovmk018@gmail.com>
2025-08-22 01:51:17 +0500

    new section +pixelperfect

--- js/script.js
+++ js/script.js
@@ -23,10 +23,38 @@ var swiper = new Swiper(".threeSwiper", {
     }
   }
 });
-var swiper = new Swiper(".mySwiper", {});
 var swiper = new Swiper(".introSwiper ", {
   pagination: {
         el: ".swiper-pagination-grey",
         clickable: true,
       },
+});
+var swiper = new Swiper(".productSwiper", {
+  loop: true,
+  spaceBetween: 30,
+  breakpoints: {
+    // mobile
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+    // planshet
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // desktop
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
 });
